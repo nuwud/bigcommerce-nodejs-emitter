309da6081baa935f7db34135460bb8944ce1b43a
gwilym <github@whoah.net>
2012-04-22 09:38:42 +1000

    Update example.js

--- example.js
+++ example.js
@@ -4,20 +4,24 @@ var bc = require('bigcommerce-emitter').create({
 	baseUrl: 'https://www.example.com/api/v2'
 });
 
+// watch the orders resource and emit some created and updated events
 bc.watch({
 	resource: '/orders',
 	create: 'order-created',
 	update: 'order-updated'
 });
 
+// as above, for customers
 bc.watch({
 	resource: '/customers',
 	create: 'customer-created',
 	update: 'customer-updated'
 });
 
+// now listen for new or updated orders or customers...
+
 bc.on('order-created', function(order){
-	// order order
+	// new order
 });
 
 bc.on('order-updated', function(order){
